02543000d36ba16e411c44e855980ca5094613e5
PiyushBarde <piyushbarde1@gmail.com>
2022-02-22 19:29:52 +0530

    ADDED: logging levels and patterns

--- src/main/java/com/bridgelabz/springaddressbookapp/SpringaddressbookappApplication.java
+++ src/main/java/com/bridgelabz/springaddressbookapp/SpringaddressbookappApplication.java
@@ -3,10 +3,14 @@ package com.bridgelabz.springaddressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SpringaddressbookappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringaddressbookappApplication.class, args);
+		log.info("Addressbook App started");
 	}
 }
