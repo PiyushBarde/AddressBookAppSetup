44cda265575cae8204c1f1356138110448678e82
PiyushBarde <piyushbarde1@gmail.com>
2022-02-22 19:58:10 +0530

    ADDED: logging by dev production and common

--- src/main/java/com/bridgelabz/springaddressbookapp/SpringaddressbookappApplication.java
+++ src/main/java/com/bridgelabz/springaddressbookapp/SpringaddressbookappApplication.java
@@ -2,11 +2,17 @@ package com.bridgelabz.springaddressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class SpringaddressbookappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringaddressbookappApplication.class, args);
+		ApplicationContext context=SpringApplication.run(SpringaddressbookappApplication.class, args);
+		log.info("Addressbook app started in the {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Addressbook app DB User is{}",context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 }
